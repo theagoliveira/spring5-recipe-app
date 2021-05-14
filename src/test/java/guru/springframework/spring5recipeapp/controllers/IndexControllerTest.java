0927dd2e0fe1c0b5483a75174253b6a44821f337
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-14 03:55:09 -0300

    Add a test for the IndexController

---
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import guru.springframework.spring5recipeapp.services.RecipeServiceImpl;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    Model model;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        assertEquals("index", indexController.getIndexPage(model));
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(recipeService, times(1)).findAll();
+    }
+
+}
