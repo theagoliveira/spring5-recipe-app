887f9566fe033e92598dbf5fe1204166e7ede384
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-14 03:40:55 -0300

    Add a JUnit unit test

---
+++ src/test/java/guru/springframework/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+}
