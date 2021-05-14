73806f312ab3299a9d91e1e013971d6ba86ca69e
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-14 03:41:22 -0300

    Add a unit test using a Mockito mock

---
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.spring5recipeapp.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void findAll() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.findAll();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
