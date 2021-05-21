fe589fd2a90578c56c99c9b7fde8d5ce00cf098a
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-21 04:48:10 -0300

    Save recipe image to database
    Changes:
    - New method for Image Service implementation
    - New tests

--- src/main/java/guru/springframework/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,12 +1,47 @@
 package guru.springframework.spring5recipeapp.services;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
-    public void saveFile(Long recipeId, MultipartFile file) {}
+    public void saveFile(Long recipeId, MultipartFile file) {
+        var optionalRecipe = recipeRepository.findById(recipeId);
+
+        if (optionalRecipe.isPresent()) {
+            try {
+                var recipe = optionalRecipe.get();
+
+                var fileBytes = file.getBytes();
+                var boxedFileBytes = new Byte[fileBytes.length];
+                var i = 0;
+                for (byte b : fileBytes) {
+                    boxedFileBytes[i++] = b;
+                }
+
+                recipe.setImage(boxedFileBytes);
+                recipeRepository.save(recipe);
+            } catch (IOException e) {
+                log.error("IO Exception occurred.", e);
+                e.printStackTrace();
+            }
+        } else {
+            log.error("Recipe with ID " + recipeId + " not found.");
+        }
+    }
 
 }
--- src/test/java/guru/springframework/spring5recipeapp/services/ImageServiceImplTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/services/ImageServiceImplTest.java
@@ -1,5 +1,56 @@
 package guru.springframework.spring5recipeapp.services;
 
-public class ImageServiceImplTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+
+class ImageServiceImplTest {
+
+    private static final Long ID = 1L;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void saveFile() throws Exception {
+        // given
+        var multipartFile = new MockMultipartFile(
+            "imagefile", "testing.txt", "text/plain", "Spring Framework Guru".getBytes()
+        );
+        var recipe = new Recipe();
+        recipe.setId(ID);
+        Optional<Recipe> optionalRecipe = Optional.of(recipe);
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        // when
+        when(recipeRepository.findById(anyLong())).thenReturn(optionalRecipe);
+        imageService.saveFile(ID, multipartFile);
+
+        // then
+        verify(recipeRepository).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
 
 }
