b324243630bb8eca6dd97093bbb8ab0b409a8fbf
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-27 05:22:44 -0300

    Add MariaDB dependency and SQL scripts
    Scripts:
    - configure-mysql: commands to create databases (development,
      production) and users (one for each DB) and grant users privileges
    - guru_database_create: commands to create the tables for each DB
      (schema)

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE sfg_dev;
-CREATE DATABASE sfg_prod;
-
 CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
 CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'guru';
+
+DROP DATABASE sfg_dev;
+DROP DATABASE sfg_prod;
+
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
 
 GRANT SELECT ON sfg_dev.* TO 'sfg_dev_user'@'localhost';
 GRANT INSERT ON sfg_dev.* TO 'sfg_dev_user'@'localhost';
